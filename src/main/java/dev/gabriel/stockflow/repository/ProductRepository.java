562945b8201d9db88a4a60350940c5025e1340dd
GabriellMartins <gabrielnpmartins@hotmail.com>
2026-06-15 23:26:21 -0300

    iniciando repository

---
+++ src/main/java/dev/gabriel/stockflow/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package dev.gabriel.stockflow.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductRepository {
+
+    @Autowired
+    private ProductRepository repository;
+
+
+}
